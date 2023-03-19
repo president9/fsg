f2146ecd8300afea82b2c5e128ca5d8fdce87095
president9 <107339442+president9@users.noreply.github.com>
2023-03-19 20:37:51 +1100

    Delete helpers.hpp

--- helpers.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "classes.h"
-
-void showInstructions();
-void winnerMessage();
-string nextPlayerPrompt(int i);
-void gameLoop(Player& example);
-void checkWinner(vector<int> &p1, vector<int> &p2);
-
